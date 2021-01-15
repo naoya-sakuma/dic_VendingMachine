a26c03f6def812b93c7c49d2795179a964ca2408
naoyasakuma <4869ns@gmail.com>
2021-01-15 15:04:21 +0900

    [clean]余分なメソッドを削除

--- vending-machine.rb
+++ vending-machine.rb
@@ -61,16 +61,6 @@ class VendingMachine
     @slot_money = 0
   end
 
-  def show_stock
-    p "コーラは#{@coke[:stocks]}本です"
-    p "レッドブルは#{@redbull[:stocks]}本です"
-    p "水は#{@water[:stocks]}本です"
-  end
-
-  def check_sales
-    @sales
-  end
-
   def buyable_juice_list
     @juice_lists.each do |juice|
       if juice[:stocks] == 0
@@ -79,22 +69,5 @@ class VendingMachine
         puts "#{juice[:name]}：販売中"
       end
     end
-  end
-
-  def add_stocks
-    puts 'どのジュースを補充しますか？'
-    puts '1：コーラ'
-    puts '2：レッドブル'
-    puts '3：水'
-    select_juice_to_stock = gets.to_i
-      if select_juice_to_stock  == 1
-        @coke[:stocks] += 5
-      elsif select_juice_to_stock  == 2
-        @redbull[:stocks] += 5
-      elsif select_juice_to_stock  == 3
-        @water[:stocks] += 5
-      else
-        puts '何も補充していません'
-      end
   end
 end
