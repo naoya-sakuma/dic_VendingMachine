86d8203dfebafa631afeb47a7d95313c9b24e268
naoyasakuma <4869ns@gmail.com>
2021-01-17 20:54:13 +0900

    [update]SalesManagerクラスを修正

--- managers_as_classs/sales_manager.rb
+++ managers_as_classs/sales_manager.rb
@@ -1,60 +1,52 @@
 class SalesManager
-  
-  def slot_money(money)
-    return false unless MONEY.include?(money)
-    @slot_money += money
+  MONEY = [10, 50, 100, 500, 1000].freeze
+
+  def buyable_juice_list
+    @juice_lists.each do |juice|
+      if juice[:stocks] == 0
+        puts "#{juice[:name]}：売切中"
+      elsif @slot_money <= juice[:price]
+        lack_amount = juice[:price] - @slot_money
+        puts "#{juice[:name]}：#{juice[:price]}円：#{lack_amount}円足りません"
+      else
+        puts "#{juice[:name]}：#{juice[:price]}円：販売中"
+      end
+    end
+  end
+
+  def slot_money
+    puts 'いくら入れますか？'
+    puts "#{MONEY}"
+    money = gets.to_i
+    if MONEY.include?(money)
+      @slot_money += money
+      puts "#{money}円投入されました"
+    else
+      puts "取り扱いできません。#{money}円のお返しです。"
+    end
   end
 
   def current_slot_money
     @slot_money
   end
 
-  def return_money
-    puts @slot_money
-    @slot_money = 0
+  def purchase(user_action)
+    product_number = user_action
+    selected_juice = @juice_lists[product_number]
+    if @slot_money >= selected_juice[:price] && selected_juice[:stocks] >= 1
+      selected_juice[:stocks] -= 1
+      @slot_money = @slot_money - selected_juice[:price]
+      @sales = @sales + selected_juice[:price]
+      puts "#{selected_juice[:name]}です。\n残金は#{current_slot_money}円です。"
+    elsif @slot_money < selected_juice[:price]
+      puts 'お金が足りません'
+    else
+      puts "#{selected_juice[:name]}は売切中です。"
+    end
   end
 
-  def purchase
-    puts '購入しますか？'
-    puts '1：コーラを購入する'
-    puts '2：レッドブルを購入する'
-    puts '3：水を購入する'
-    users_action = gets.to_i
-    if users_action == 1
-      if @slot_money >= @coke[:price] && @coke[:stocks] > 1
-        @coke[:stocks] -= 1
-        @slot_money = @slot_money - @coke[:price]
-        @sales = @sales + @coke[:price]
-        puts 'コーラです'
-        return_money
-      else
-        puts '購入できません'
-        return_money
-      end
-    elsif users_action == 2
-      if @slot_money >= @redbull[:price] && @redbull[:stocks] > 1
-        @redbull[:stocks] -= 1
-        @slot_money = @slot_money - @redbull[:price]
-        @sales = @sales + @redbull[:price]
-        puts 'レッドブルです'
-        return_money
-      else
-        puts '購入できません'
-        return_money
-      end
-    elsif users_action == 3
-      if @slot_money >= @water[:price] && @water[:stocks] > 1
-        @water[:stocks] -= 1
-        @slot_money = @slot_money - @water[:price]
-        @sales = @sales + @water[:price]
-        puts '水です'
-        return_money
-      else
-        puts '購入できません'
-        return_money
-      end
-    else
-      puts "無効な入力です"
-    end
+  def return_money
+    puts "#{@slot_money}円のお返しです。"
+    @slot_money = 0
   end
 end
