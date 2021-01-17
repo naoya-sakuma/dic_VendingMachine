7e6c9ffdc7d8b52d162be4fbdbbb6fe004896e30
naoyasakuma <4869ns@gmail.com>
2021-01-17 20:48:16 +0900

    [fix]在庫追加機能のメソッドを修正

--- managers_as_modules/stocks_manager.rb
+++ managers_as_modules/stocks_manager.rb
@@ -2,11 +2,14 @@ module StocksManager
   def add_stocks
     puts 'どのジュースを補充しますか？'
     puts "0：コーラ\n1：レッドブル\n2：水"
-    input_number = gets.to_i
-    return "0 〜 2の数字を入力してください" unless [0, 1, 2].include?(input_number)
-    juice_to_stock = @juice_lists[input_number]
-    juice_to_stock[:stocks] += 5
-    puts "#{juice_to_stock[:name]}を5本追加しました。"
+    product_number = gets.to_i
+    if [0, 1, 2].include?(product_number)
+      juice_to_stock = @juice_lists[product_number]
+      juice_to_stock[:stocks] += 5
+      puts "#{juice_to_stock[:name]}を5本追加しました。"
+    else
+      puts "0〜2の数字を入力してください。"
+    end
   end
 
   def check_stocks
